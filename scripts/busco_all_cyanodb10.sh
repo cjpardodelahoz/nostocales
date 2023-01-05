4a378403e39a44248e58b16a836fb8a684cdca41
cjpardodelahoz <cjpardodelahoz@gmail.com>
2023-01-05 15:24:40 -0500

    busco on all genomes with cyano_odb10

---
+++ scripts/busco_all_cyanodb10.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --array=1-220
+#SBATCH --mem-per-cpu=10G  # adjust as needed
+#SBATCH -c 16 # number of threads per process
+#SBATCH --output=log/busco_all_cyanodb10_%A_%a.out
+#SBATCH --error=log/busco_all_cyanodb10_%A_%a.err
+#SBATCH --partition=scavenger
+
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate busco
+
+S=$(cat misc_files/genome_ids_all.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
+
+busco -i analyses/cyano_genomes/set103/${S} -l cyanobacteria_odb10 -m genome \
+--out analyses/genome_qc/busco/all_cyanodb10/by_taxon/${S} \
+--offline --download_path misc_files/busco_downloads
