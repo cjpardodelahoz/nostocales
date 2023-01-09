ffab6632a02eabc394b1d7668887a94960cfd753
lutzonilab <cjpardodelahoz@gmail.com>
2023-01-09 12:31:42 -0500

    added absolute path so raxml doesn't complain

--- scripts/prelim_concat_tree.sh
+++ scripts/prelim_concat_tree.sh
@@ -2,10 +2,13 @@
 
 #SBATCH --mem-per-cpu=2G  # adjust as needed
 #SBATCH -c 16 # number of threads per process
-#SBATCH --output=prelim_concat_tree.out
-#SBATCH --error=prelim_concat_tree.err
+#SBATCH --output=log/prelim_concat_tree.out
+#SBATCH --error=log/prelim_concat_tree.err
 #SBATCH --partition=common
 
+# Path to wd. Replace with the path where you have the nostocales project dir
+# This is because RAxML doesn't like relative paths
+wd="/hpc/group/bio1/nostocales"
 # Load RAxML module
 module load RAxML/8.2.12
 # Direcotry for output
@@ -14,4 +17,4 @@ mkdir -p analyses/prelim/trees/concat
 raxmlHPC-PTHREADS-SSE3 -T 16 -n concat_aa.tree \
  -s analyses/prelim/alignments/concat/concat_aa.phy \
  -c 25 -m PROTCATWAG -p 12345 -f a -N 1000 -x 12345 \
- --asc-corr lewis -w analyses/prelim/trees/concat
+ --asc-corr lewis -w ${wd}/analyses/prelim/trees/concat
