52c41addd3d0a9f4f6c103889151491d630dde03
lutzonilab <cjpardodelahoz@gmail.com>
2023-01-09 11:25:36 -0500

    corrected path to taxa_passed_qc.txt

--- scripts/sort_busco_seqs_all_cyanodb10.R
+++ scripts/sort_busco_seqs_all_cyanodb10.R
@@ -6,7 +6,7 @@ library(phylotools)
 source("scripts/r_functions.R")
 
 # Load genome ids that passed initial qc
-sample_ids <- scan("taxa_passed_qc.txt", what = "character")
+sample_ids <- scan("misc_files/taxa_passed_qc.txt", what = "character")
 # Get busco na seq files
 sort_busco_seqs(sample_ids = sample_ids,
                 busco_out_dir = "analyses/genome_qc/busco/all_cyanodb10/by_taxon", 
