5e9acba418598c64727b04331215974c1ffe44e1
lutzonilab <cjpardodelahoz@gmail.com>
2023-01-10 20:04:16 -0500

    raxml script for prelim concat tree changed to run on mpi

--- scripts/prelim_concat_tree.sh
+++ scripts/prelim_concat_tree.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #SBATCH --mem-per-cpu=2G  # adjust as needed
-#SBATCH -c 16 # number of threads per process
+#SBATCH -c 8 # number of threads per process
+#SBATCH -n 12 # number of MPI processes
 #SBATCH --output=log/prelim_concat_tree.out
 #SBATCH --error=log/prelim_concat_tree.err
 #SBATCH --partition=common
@@ -10,11 +11,11 @@
 # This is because RAxML doesn't like relative paths
 wd="/hpc/group/bio1/nostocales"
 # Load RAxML module
-module load RAxML/8.2.12
+module load RAxML/8.2.12-hybrid
 # Direcotry for output
 mkdir -p analyses/prelim/trees/concat
 # Run RAxML
-raxmlHPC-PTHREADS-SSE3 -T 16 -n concat_aa.tree \
+mpirun -n 12 raxmlHPC-HYBRID-SSE3 -T 8 -n concat_aa.tree \
  -s analyses/prelim/alignments/concat/concat_aa.phy \
  -c 25 -m PROTCATWAG -p 12345 -f a -N 1000 -x 12345 \
  --asc-corr lewis -w ${wd}/analyses/prelim/trees/concat
