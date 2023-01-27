4f3ba45bf577675aef46b852034fd81f1b00a9da
lutzonilab <cjpardodelahoz@gmail.com>
2023-01-27 16:44:57 -0500

    included sorting script for L746 and added code to make loci names have the _selected_55 suffix and match the orignal files and scripts

--- commands.sh
+++ commands.sh
@@ -117,6 +117,18 @@ sbatch scripts/L70_astral10_ng_aa.sh
 
 # L746
 
+# Get sequence files for each of the busco loci
+# This script will create the busco sequence files in analyses/L746/seqs
+# It will run faster with higher memory (peak at ~32 GB RAM)
+Rscript scripts/sort_busco_seqs_cyanodb10_subset0.R
+# Add the "selected_55"" suffix to the seqs 
+for locus in $(cat misc_files/L746.txt) ; do
+ mv analyses/L746/seqs/${locus}.fna analyses/L746/seqs/${locus}_selected_55.fna
+ mv analyses/L746/seqs/${locus}.faa analyses/L746/seqs/${locus}_selected_55.faa
+done
+# Add the "selected_55" suffix to the seqs and the loci identifiers
+sed -i.bak "s|$|_selected_55|" misc_files/L746.txt
+rm misc_files/L746.txt.bak
 # AA alignments
 # This will write the alignments to analyses/L746/alignments/single
 sbatch scripts/mafft_L746_subset0_aa.sh
@@ -168,9 +180,56 @@ sbatch busco_all_nostocalesdb10.sh
 # misc_files/L1648.txt          The list of busco loci part of the L1648 dataset
 # It also contains code for a couple of histograms showing the 
 # distribution of taxa and loci.
-Rscript scripts/filter_loci_nostocalesdb10.R # NEEDS TO BE TESTED WHEN I COPY THE SUMMARY FILE
+Rscript scripts/filter_loci_nostocalesdb10_subset0.R # NEEDS TO BE TESTED WHEN I COPY THE SUMMARY FILE
 # Get sequence files for each of the busco loci
 # This script will create the busco sequence files in analyses/L1648/seqs
 # It will run faster with higher memory (peak at ~32 GB RAM)
 Rscript scripts/sort_busco_seqs_all_cyanodb10.R # NEEDS TO BE TESTED WHEN I COPY THE BUSCO OUTPUT
-
+# Add the "selected_55"" suffix to the seqs 
+for locus in $(cat misc_files/L1648.txt) ; do
+ mv analyses/L1648/seqs/${locus}.fna analyses/L1648/seqs/${locus}_selected_55.fna
+ mv analyses/L1648/seqs/${locus}.faa analyses/L1648/seqs/${locus}_selected_55.faa
+done
+# Add the "selected_55" suffix to the seqs and the loci identifiers
+sed -i.bak "s|$|_selected_55|" misc_files/L1648.txt
+# AA alignments
+# This will write the alignments to analyses/L746/alignments/single
+sbatch scripts/mafft_L746_subset0_aa.sh
+# NA alignments with PAL2NAL
+# This will align the nucleotides using the amino acid aligment as a guideline
+# and will write the alignments to analyses/L70/alignments/single
+sbatch scripts/pal2nal_L746_subset0.sh
+# Trim gaps from alignments using trimal
+# This will write trimmmed alignments to analyses/L70/alignments/single
+sbatch scripts/trimal_L746_subset0_ng.sh
+# Remove trimal crap from headers
+sbatch scripts/fix_headers_L746_subset0.sh
+# Get codon partition file for nucleotide alignments
+# Codon partition files will be written to analyses/L70/alignments/single/${locus}_ng_Cpart
+scripts/get_codon_partition_L746_subset0.sh
+# Run ML gene trees with iqtree
+# All iqtree output will be written to analyses/L70/trees/single/aa|na
+# The tree file will be analyses/L70/trees/single/aa|na/${locus}_seelcted_55_ng.treefile
+sbatch scripts/ml_gene_trees_L746_subset0_aa.sh
+sbatch scripts/ml_gene_trees_L746_subset0_na.sh
+# Concatenate sequences
+# Concatenated alignments will be in analyses/L70/alignments/concat
+sbatch scripts/concatenate_L746_subset0.sh
+# Get codon partition file for the na concatenated alignments
+# Codon partitions will be in analyses/L70/alignments/concat/na/ng_na_Cpart
+scripts/get_codon_partition_concat_ng_L746.sh
+# Infer aa concatenated guide tree for PMSF
+sbatch scripts/L746_concat_guide_ng.sh
+# Infer aa concatenated PMSF tree
+# The pmsf tree will be in analyses/L70/trees/concat/aa/ng_concat_pmsf.treefile
+sbatch scripts/L746_concat_pmsf_ng.sh
+# Run partition finder for na concat alignment within iqtree
+# The partition and model file will be in analyses/L70/trees/concat/na/ng_concat.best_scheme.nex
+sbatch scripts/L746_concat_pf_ng_na.sh
+# Infer ml concat tree for na using partitions from pf
+# The tree will be in analyses/L70/trees/concat/na/ng_concat_pfml.treefile
+sbatch scripts/L746_concat_pfml_ng_na.sh
+# Run ASTRAL on gene trees with branches < %10 UFBoot collapsed
+# The astral trees wimm be in analyses/L70/trees/astral/aa|na/ng_astral.tre
+sbatch scripts/L746_astral10_ng_na.sh
+sbatch scripts/L746_astral10_ng_aa.sh
