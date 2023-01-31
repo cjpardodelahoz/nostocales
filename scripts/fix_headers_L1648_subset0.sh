5848db9c095f22319e2c14caff682502171c176f
lutzonilab <cjpardodelahoz@gmail.com>
2023-01-31 16:55:56 -0500

    fix headers for L1648+ng. Needs to include strict also

---
+++ scripts/fix_headers_L1648_subset0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --array=1-1648
+#SBATCH --mem-per-cpu=1G  # adjust as needed
+#SBATCH -c 1 # number of threads per process
+#SBATCH --output=log/fix_headers_L1648_%A_%a.out
+#SBATCH --error=log/fix_headers_L1648_%A_%a.err
+#SBATCH --partition=scavenger
+
+locus=$(cat misc_files/L1648.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
+
+sed -i 's/\s.*//' analyses/L1648/alignments/single/${locus}_ng.faa
+sed -i 's/\s.*//' analyses/L1648/alignments/single/${locus}_ng.fna
