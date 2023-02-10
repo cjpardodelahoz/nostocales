7ce4857c83cff528a9d3bea0a0f0cb4b2828aae2
cjpardodelahoz <cjpardodelahoz@gmail.com>
2023-02-10 18:34:09 -0500

    added file with list loci used for subset 1 to use in arrays

--- scripts/get_loci_to_rm_subset1.R
+++ scripts/get_loci_to_rm_subset1.R
@@ -1,8 +1,15 @@
 #!/usr/bin/env Rscript
 
+library(stringr)
+
 # Load alignment summaries for taxa subset 1
 subset1_aa_ng_summary <- read.delim("analyses/phylonetworks/alignment_summaries/subset1_ng_aln_aa.summary", row.names = NULL)
 # Get list of loci to remove
 subset1_loci_rm <- subset(subset1_aa_ng_summary, No_of_taxa < 12)$Alignment_name
 # Write file with loci to remove (with <12 taxa)
 write(subset1_loci_rm, file = "misc_files/subset1_loci_rm.txt")
+# Get list of loci kept for subset1
+subset1_loci <- subset(subset1_aa_ng_summary, No_of_taxa >= 12)$Alignment_name
+subset1_loci <- str_remove(subset1_loci, ".phy")
+# Write file with loci used for subset 1
+write(subset1_loci, file = "misc_files/subset1_loci.txt")
